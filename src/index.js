d367dea71922e80839e93fe7372f8a5c983d0d4a
DaveBitter <d.bitter@viriciti.com>
2018-01-15 21:02:44 +0100

    additional code comments

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './js/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Mounting the main react component in the DOM
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/js/components/molecules/StatBar.js
+++ src/js/components/molecules/StatBar.js
@@ -14,6 +14,7 @@ class StatBar extends Component {
   };
 
   componentWillReceiveProps(nextProps) {
+    // Only update the data in the current state if it has changed
     if (this.state.data !== nextProps.data) {
       this.setState({ data: nextProps.data }, () =>
         nextProps.datapoints.map(datapoint => this.getStats(datapoint))
@@ -21,32 +22,43 @@ class StatBar extends Component {
     }
   }
 
+  // TODO: move this logic up to App.js to keep this component dumb
   getStats(datapoint) {
+    // Create a copy of the the data and stats in the current state
     const data = [].concat(this.state.data);
     const stats = Object.assign(this.state.stats, {});
     let value = '-';
+
     switch (datapoint.key) {
       case 'countries':
+        // Get the length of the countries which is the amount amount a countries
         value = data.length;
         break;
       case 'population':
+        // Reduce the population of all the countries to a single value
         value = data.reduce((total, next) => total + next.population, 0);
         break;
       case 'area':
+        // Reduce the area of all the countries that have it in the data to a single value
         value = data
           .filter(datum => datum.area)
           .reduce((total, next) => total + next.area, 0);
         break;
       case 'languages':
+        // Create a copy of the all the languages for all the countries
+        // and create a single level array out of it by using spreak
         const languages = [].concat(...data.map(datum => datum.languages));
+        // Remove all the duplicate languages
         value = uniq(languages, language => language.name).length;
         break;
       default:
         break;
     }
 
+    // Update the value in the stats object for the datapoint
     stats[datapoint.key] = value;
 
+    // Update the stats object in the current state with the updated stats object
     this.setState({ stats });
   }
 
