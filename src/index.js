4bae9e54f73a78c7f7324ad3e02659f4f762cbaa
Dave Bitter <dbitter@mirabeau.nl>
2019-07-10 09:42:11 +0200

    fix(object-keys): Added polyfill for IE11

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './js/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Polyfills
+if (!Object.values) Object.values = o => Object.keys(o).map(k => o[k]);
+
+
 // Mounting the main react component in the DOM
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
